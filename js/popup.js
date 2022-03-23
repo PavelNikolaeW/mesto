46a79374e1c1efce12be33f271302f69e6885410
PavelNikolaeW <pavel19.nikolaev93@gmail.com>
2022-03-23 13:54:46 +0300

    feat: add popup

---
+++ js/popup.js
@@ -0,0 +1,51 @@
+let popupLinks = document.querySelectorAll('.popup-link');
+let personName = document.querySelector('.person__name');
+let subText = document.querySelector('.person__subtext');
+let popupCloses = document.querySelectorAll('.close-popup');
+let popupForm = document.querySelector('.popup__form');
+let formSubmit = popupForm.querySelector('.popup__submit');
+let prfixPopup = 'popup-';
+
+if (popupLinks.length > 0) {
+    for (let i = 0; i < popupLinks.length; i++) {
+        let link = popupLinks[i];
+        link.addEventListener('click', function(e) {
+            let popupId = link.getAttribute('id');
+            let popup = document.getElementById(prfixPopup + popupId);
+            let popupName = popup.querySelector('.name');
+            let popupSubTexet = popup.querySelector('.subtext');
+            if (popupName != null) {
+                popupName.setAttribute('value', personName.textContent);
+            }
+            if (popupSubTexet != null) {
+                popupSubTexet.setAttribute('value', subText.textContent);
+            }
+            popup.classList.add('open');
+            e.preventDefault()
+        })
+    }
+}
+
+if (popupCloses.length > 0) {
+    for (let i = 0; i < popupCloses.length; i++) {
+        let close = popupCloses[i];
+        close.addEventListener('click', function(e) {
+            let popup = close.closest('.popup')
+            popup.classList.remove('open');
+            e.preventDefault();
+        })
+    }
+}
+
+if (formSubmit != null) {
+    formSubmit.addEventListener('click', function(e) {
+        let inputName = popupForm.querySelector('.name');
+        let inputSubText = popupForm.querySelector('.subtext');
+        personName.textContent = inputName.value;
+        subText.textContent = inputSubText.value;
+
+        let popup = popupForm.closest('.popup');
+        popup.classList.remove('open');
+        e.preventDefault();
+    })
+}
