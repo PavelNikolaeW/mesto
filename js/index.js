922e24ffbb1b17065e51863ada4a9610bdd5864d
PavelNikolaeW <pavel19.nikolaev93@gmail.com>
2022-03-28 12:53:10 +0300

    fix all

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
-function include(url) {
-    var script = document.createElement('script');
-    script.src = url;
-    document.getElementsByTagName('body')[0].appendChild(script);
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    nameProfile.textContent = nameInput.value;
+    subTextProfile.textContent = subTextInput.value;
+    openClosePopup(evt);
 }
 
-include("./js/popup.js");
-include("./js/like.js");
+function openClosePopup(evt) {
+    console.log(popup);
+    evt.preventDefault();
+    popup.classList.toggle('popup_opened');
+}
+
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_name');
+let subTextInput = formElement.querySelector('.popup__input_subtext');
+
+let nameProfile = document.querySelector('.person__name');
+let subTextProfile = document.querySelector('.person__subtext');
+
+nameInput.value = nameProfile.innerText;
+subTextInput.value = subTextProfile.innerText;
+
+let personButton = document.querySelector('.person__button');
+let popup = document.querySelector('.popup');
+let closePopup = popup.querySelector('.popup__close');
+
+
+personButton.addEventListener('click', openClosePopup);
+closePopup.addEventListener('click', openClosePopup);
+formElement.addEventListener('submit', formSubmitHandler);
